a464c89a11c5c8c499455d1301d5ff375268a7ea
breeze303 <122870904+breeze303@users.noreply.github.com>
2023-12-30 15:07:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ build_firmware() {
   ./scripts/feeds update -a
   ./scripts/feeds install -a
 
-  cp ${GITHUB_WORKSPACE}/config/${BUILD_PROFILE} .config
+  cp ${GITHUB_WORKSPACE}/config/test.config .config
   make defconfig
   make -j$(($(nproc) + 1)) V=e || make -j1 V=sc || exit 1
 
